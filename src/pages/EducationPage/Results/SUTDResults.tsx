2a146e0aa2a93476c8bb82d9313926f65329141a
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2025-01-06 13:07:33 +0800

    Updated term 3 results for SUTD

--- src/pages/EducationPage/Results/SUTDResults.tsx
+++ src/pages/EducationPage/Results/SUTDResults.tsx
@@ -3,11 +3,11 @@ import { Results, Subject } from "./Results";
 const categorisedSubjects = [
   {
     category: "Overall",
-    subjects: [new Subject("Cumulative GPA", "4.63/5.00")],
+    subjects: [new Subject("Cumulative GPA", "4.70/5.00")],
   },
   {
     category: "Term 3",
-    subjects: [new Subject("GPA", "4.67/5.00")],
+    subjects: [new Subject("GPA", "4.75/5.00")],
   },
   {
     category: "Term 2",
--- src/pages/ProjectsPage/Projects/CysecProjects.tsx
+++ src/pages/ProjectsPage/Projects/CysecProjects.tsx
@@ -4,8 +4,15 @@ const KaliLinuxFamiliarisation = {
   name: "Kali Linux familiarisation",
   achievements: `Familiarised myself with the Kali Linux OS. Learnt how to use 
   1) Information gathering tools
-  - dig, dnsenum (Zone transfer attacks)
-  - lbd (IPS/IDS evasion)`,
+  - dig, dnsenum, host, dnsrecon (Zone transfer attacks)
+  - lbd, wafw00f, hping3 (IPS/IDS detection)
+  - arping, fping, arp-scan, hping3, masscan, atk6-thcping6, nmap, netdiscover, nbtscan (Live Host Identification)
+  - (OSINT analysis)
+  - traceroute, mtr (Route analysis)
+  - snmpwalk, nmap, snmp, hping3 (Device discovery)
+  - (SMB attacks)
+  2) 
+  `,
 };
 
 const projects = [KaliLinuxFamiliarisation];
