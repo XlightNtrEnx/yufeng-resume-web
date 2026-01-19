defbb3e037644cc9a0688cc9055d4b653e6c81d6
b6Ah_SHG-262-Wb <xyf.oco@gmail.com>
2026-01-20 03:43:08 +0800

    poop

--- src/provider/IDBProvider/IDBProvider.tsx
+++ src/provider/IDBProvider/IDBProvider.tsx
@@ -1,7 +1,7 @@
-import { deleteDB, openDB } from "idb";
+import { LoadingSpinner } from "@src/common/component/LoadingSpinner";
+import { openDB } from "idb";
 import React, { createContext, useEffect, useState } from "react";
 import { CustomDBSchemaExtended, CustomIDBPDatabase } from "./db";
-import { LoadingSpinner } from "@src/common/component/LoadingSpinner";
 
 class IDB {
   db: CustomIDBPDatabase;
@@ -13,31 +13,26 @@ export const IDBProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     const dbName = "api";
-    deleteDB(dbName, {
-      blocked(currentVersion, event) {
-        console.log("Blocked from deleting db");
-      },
-    }).then(() => {
-      console.log("Deleted db");
-      openDB<CustomDBSchemaExtended>(dbName, 1, {
-        upgrade(db, oldVersion, newVersion, transaction, event) {
-          const postStore = db.createObjectStore("posts", { keyPath: "id" });
-          postStore.createIndex("preview_id", "preview_id");
+    openDB<CustomDBSchemaExtended>(dbName, 1, {
+      upgrade(db, oldVersion, newVersion, transaction, event) {
+        const postStore = db.createObjectStore("posts", { keyPath: "id" });
+        postStore.createIndex("preview_id", "preview_id");
 
-          const previewStore = db.createObjectStore("previews", {
-            keyPath: "id",
-          });
-          previewStore.createIndex("type", "type");
-          console.log("Upgraded db");
-        },
-        blocked(currentVersion, blockedVersion, event) {},
-        blocking(currentVersion, blockedVersion, event) {},
-        terminated() {},
-      }).then((db) => {
-        const idbObject = new IDB();
-        idbObject.db = db;
-        setIDBValue(idbObject);
-      });
+        const previewStore = db.createObjectStore("previews", {
+          keyPath: "id",
+        });
+        previewStore.createIndex("type", "type");
+        console.log("Upgraded db");
+      },
+      blocked(currentVersion, blockedVersion, event) {},
+      blocking(currentVersion, blockedVersion, event) {},
+      terminated() {},
+    }).then((db) => {
+      db.clear("posts");
+      db.clear("previews");
+      const idbObject = new IDB();
+      idbObject.db = db;
+      setIDBValue(idbObject);
     });
   }, []);
 
